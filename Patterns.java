5caba45f0a4b2d537a9b4eb6db68d07554710222
haleyprindle <haley@prindle.net>
2025-10-30 18:10:14 +0000

    ll

---
+++ Patterns.java
@@ -0,0 +1,76 @@
+//
+//  Class author:  Haley Prindle
+//  Date created: 10/30/25
+//  General description: a brief summary of what this particular class does.
+public class Patterns{
+
+//
+//  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
+//  Post-condition: what the result of executing this method will be.
+//
+public static void stars(int rows){
+    int i = 1;
+    int startNumber = 1;
+    while (i<=rows)
+    {
+        if(rows>0)
+            {
+            while(startNumber<=rows)
+        {
+           System.out.println("*");
+           startNumber++;
+          
+        } 
+        i++;
+            }
+        else
+        System.out.println("");
+       
+    }
+}
+public static void main(String[] args){
+
+    stars(5);
+}
+
+
+
+//
+//  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
+//  Post-condition: what the result of executing this method will be.
+//
+public static void triangle(int rows){
+    
+}
+
+
+//
+//  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
+//  Post-condition: what the result of executing this method will be.
+//
+public static void odds(int start){
+
+
+}
+
+
+//
+//  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
+//  Post-condition: what the result of executing this method will be.
+//
+public static void eo(int maxE){
+
+
+}
+
+
+//
+//  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
+//  Post-condition: what the result of executing this method will be.
+//
+public static void pyramid(int rows){
+
+}
+}
+
+
