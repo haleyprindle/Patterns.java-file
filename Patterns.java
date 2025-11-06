78e38163a905de248d55e51ee807eb567e0f014d
haleyprindle <haley@prindle.net>
2025-11-06 19:29:18 +0000

    dbvbcvxz

--- Patterns.java
+++ Patterns.java
@@ -5,8 +5,10 @@
 public class Patterns{
 
 //
-//  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
-//  Post-condition: what the result of executing this method will be.
+//  Pre-condition: The number of rows inputed has to be a number for the method to work. 
+// the numbrs has to be greater than 0 for there to be any stars printed.
+//  Post-condition: the result is a stack of stars that print out int rows number of rows. Each row has two more stars than the last. 
+// the first row starts with one star.
 //
 
 
@@ -30,20 +32,33 @@ public static void stars(int rows){
 
 
 
-public static void main(String[] args){
 
-    stars(5);
-    return;
-}
 
 
 
 //
-//  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
-//  Post-condition: what the result of executing this method will be.
+//  Pre-condition: The input for the variable rows has to be greater than 0 for anything to be printed out.
+//Also the input has to be a number for the method to work.
+//  Post-condition: The result is a stack of numbers with the number of rows that was inputed. 
+//Each row should print out the number of that row, rows times. 
+//ex. inputing 5 for rows should output row one to be: 1, row two to be: 22, row three to be: 333, etc, printing a total of five rows 
 //
 public static void triangle(int rows){
-    
+   int currentRow = 1;
+    int number = 1;
+
+   while (currentRow <= rows) {
+            int triangleCount = 1;
+
+            while (triangleCount <= number) {
+                
+                System.out.print(number);
+                triangleCount++;
+            }
+            number++;
+            System.out.println(); 
+            currentRow++;
+        } 
 }
 
 
@@ -53,11 +68,34 @@ public static void triangle(int rows){
 //  Post-condition: what the result of executing this method will be.
 //
 public static void odds(int start){
+        
+    for(int i =start; i>0;i--){
+        if(start%2!=0){
+            int number =start;
+            for(int j =number;j>0;){
+                System.out.print(number);
+                   j=-2;
+        }
+        number = (start/2)+3;
+        System.out.println(); 
+        }
+        else
+        {
+            start-=start;
+        }
+}
 
+    
 
 }
 
+public static void main(String[] args){
 
+    stars(5);
+    triangle(5);
+    odds(9);
+    return;
+}
 //
 //  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
 //  Post-condition: what the result of executing this method will be.
